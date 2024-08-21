1c1dbb4578fb01eff26ff5e6551c5955ddc49581
dywkat <katya.kolosovaaa@gmail.com>
2024-08-21 11:19:51 +0300

    Сверстать окно добавления суммной налички

--- app/src/main/java/com/example/accountingexpensesincomebeautysalon/AccountingActivity.kt
+++ app/src/main/java/com/example/accountingexpensesincomebeautysalon/AccountingActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Add_expense : AppCompatActivity() {
+class AccountingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_expense)
+        setContentView(R.layout.activity_accounting)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
