84bc400cbd3ba8c9375f6f6353a7891caa2340a9
dywkat <katya.kolosovaaa@gmail.com>
2024-08-22 12:52:47 +0300

    Добавить переход с окна MainActivity на AccountingActivity и проверку при входе

--- app/src/main/java/com/example/accountingexpensesincomebeautysalon/MainActivity.kt
+++ app/src/main/java/com/example/accountingexpensesincomebeautysalon/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.accountingexpensesincomebeautysalon
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +20,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val userIdSalon = findViewById<Spinner>(R.id.ds_admin_list)
+        val userPasswordSalon = findViewById<EditText>(R.id.editText_password)
+        val buttonPassword = findViewById<Button>(R.id.password)
+
+        buttonPassword.setOnClickListener(){
+            val idSalon = userIdSalon.selectedItem
+            val passwordSalon = userPasswordSalon.text.toString().trim()
+
+            if (idSalon == null || passwordSalon == "")
+                Toast.makeText(this, "Введите пароль и выберите салон", Toast.LENGTH_LONG).show()
+            else {
+                val intent = Intent(this@MainActivity, AccountingActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
